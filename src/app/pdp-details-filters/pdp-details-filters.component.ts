c96b51582c0425ab0163f97ec679c8d04c713316
Pruthviraj Patel <pruthviraj@lendingkart.com>
2019-11-15 19:58:11 +0530

    Sizes filter

--- src/app/pdp-details-filters/pdp-details-filters.component.ts
+++ src/app/pdp-details-filters/pdp-details-filters.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PdpDetailsFiltersComponent implements OnInit {
   selectedColor = 'blue.png';
+  sizes = [5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13];
   colors = [
     'semi-brown.png',
     'light-brown.png',
