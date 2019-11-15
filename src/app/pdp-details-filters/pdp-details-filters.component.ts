4b90cbeea868b09d74d5d8e4b3c971bcf62207c0
Pruthviraj Patel <pruthviraj@lendingkart.com>
2019-11-15 18:57:02 +0530

    Color selection html

--- src/app/pdp-details-filters/pdp-details-filters.component.ts
+++ src/app/pdp-details-filters/pdp-details-filters.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pdp-details-filters.component.scss']
 })
 export class PdpDetailsFiltersComponent implements OnInit {
-
+  selectedColor = 'blue.png';
+  colors = [
+    'semi-brown.png',
+    'light-brown.png',
+    'violet.png',
+    'blue.png',
+    'brown.png',
+    'light-blue.png',
+    'black.png',
+    'semi-brown.png',
+    'light-brown.png',
+    'violet.png',
+    'brown.png'
+  ];
   constructor() { }
 
   ngOnInit() {
